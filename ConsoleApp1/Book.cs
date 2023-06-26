e80ac0b81f6def7e55de548556358bb515c7dd4e
leviethoangg <111514749+leviethoangg@users.noreply.github.com>
2023-06-26 15:53:54 +0700

    rthrgtgt
    gtge

---
+++ ConsoleApp1/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp1;
+
+namespace Exam
+{
+    class Book : Product
+    {
+        public Book(string productId, string name, double price, string producer)
+            : base(productId, name, price, producer)
+        {
+
+        }
+
+        public override double ComputeTax()
+        {
+            return Price * 0.08;
+        }
+    }
+}
---
+++ ConsoleApp1/MobilePhone.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp1;
+
+namespace Exam
+{
+    class MobilePhone : Product
+    {
+        public MobilePhone(string productId, string name, double price, string producer)
+            : base(productId, name, price, producer)
+        {
+        }
+
+        public override double ComputeTax()
+        {
+            return Price * 0.1;
+        }
+    }
+
+}
